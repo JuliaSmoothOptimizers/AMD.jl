6d1f4c278e9c31ab0eb8c88fc58a294bcf119eb4
Dominique Orban <dominique.orban@gmail.com>
2015-10-10 18:04:48 -0400

    Tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,17 @@
 using AMD
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+for n in [10, 20, 30]
+  for density in [.25, .75, 1.0]
+    for T in [Cint, Clong]
+      A = convert(SparseMatrixCSC{Float64,T}, sprand(n, n, density))
+      @assert amd_valid(A)
+
+      meta = Amd{T}()
+      p = amd(A, meta)
+      @assert meta.info[AMD_STATUS] == AMD_OK
+      @assert minimum(p) == 1
+      @assert maximum(p) == n
+    end
+  end
+end
