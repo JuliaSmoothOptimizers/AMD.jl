63d4b766e060befadc2980b37c4122084fe5dba2
Dominique Orban <dominique.orban@gmail.com>
2015-10-12 22:31:42 -0400

    Improve tests for code coverage.

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,6 +12,16 @@ for n in [10, 20, 30]
       @assert meta.info[AMD_STATUS] == AMD_OK
       @assert minimum(p) == 1
       @assert maximum(p) == n
+
+      q = amd(A)
+      @assert p == q
     end
   end
 end
+
+# For coverage.
+meta = Amd{Clong}()
+A = sprand(10, 10, .5)
+p = amd(A, meta)
+show(meta)
+print(meta)
