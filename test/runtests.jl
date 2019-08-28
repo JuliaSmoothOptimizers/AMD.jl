9dcd6cf110ffa2d9f599540aa2ffaa37defaf3c4
Dominique Orban <dominique.orban@gmail.com>
2019-08-28 17:24:41 -0400

    allow Symmetric input
    closes #23

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,20 +1,36 @@
-using AMD, Test, SparseArrays
+using LinearAlgebra
+using SparseArrays
+using Test
+
+using AMD
 
 _Clong = Base.Sys.WORD_SIZE == 32 ? Clong : Clonglong
 for n in [10, 20, 30]
   for density in [.25, .75, 1.0]
     for T in [Cint, _Clong]
       A = convert(SparseMatrixCSC{Float64,T}, sprand(n, n, density))
-      @assert amd_valid(A)
+      @test amd_valid(A)
 
       meta = Amd()
       p = amd(A, meta)
-      @assert meta.info[AMD_STATUS] == AMD_OK
-      @assert minimum(p) == 1
-      @assert maximum(p) == n
+      @test meta.info[AMD_STATUS] == AMD_OK
+      @test minimum(p) == 1
+      @test maximum(p) == n
 
       q = amd(A)
-      @assert p == q
+      @test all(p .== q)
+
+      # test that amd(A) with A unsymmetric is amd(A + A')
+      A = A + A'
+      pA = amd(A)
+
+      L = Symmetric(A, :L)
+      pL = amd(L)
+      @test all(pA .== pL)
+
+      U = Symmetric(A, :U)
+      pU = amd(U)
+      @test all(pA .== pU)
     end
   end
 end
