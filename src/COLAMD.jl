59fefa256c27e834fb9c5fd44540ef765c29ec5d
Dominique <dominique.orban@gmail.com>
2020-05-03 14:43:29 -0400

    use index shift of correct int type

--- src/COLAMD.jl
+++ src/COLAMD.jl
@@ -84,16 +84,16 @@ for (orderfn, typ) in ((:_colamd, Cint), (:_colamd_l, _Clong))
     function colamd(A::SparseMatrixCSC{F,$typ}, meta::Colamd{$typ}) where F
       nrow, ncol = size(A)
       nnz = A.colptr[end] - 1
-      p = A.colptr .- 1  # 0-based indexing
+      p = A.colptr .- $typ(1)  # 0-based indexing
       len = colamd_recommended($typ(nnz), $typ(nrow), $typ(ncol))
       workspace = zeros($typ, len)
-      workspace[1:length(A.rowval)] .= A.rowval .- 1
+      workspace[1:length(A.rowval)] .= A.rowval .- $typ(1)
       valid = ccall($orderfn, $typ,
                     ($typ, $typ, $typ, $Ptr{$typ}, Ptr{$typ}, Ptr{Cdouble},  Ptr{$typ}),
                      nrow, ncol,  len,  workspace,         p,   meta.knobs, meta.stats)
       Bool(valid) || throw("colamd status: $(colamd_statuses[meta.stats[COLAMD_STATUS]])")
       pop!(p)  # remove the number of nnz
-      p .+= 1  # 1-based indexing
+      p .+= $typ(1)  # 1-based indexing
       return p
     end
 
@@ -127,15 +127,15 @@ for (fn, typ) in ((:_symamd, Cint), (:_symamd_l, _Clong))
 
     function symamd(A::SparseMatrixCSC{F,$typ}, meta::Colamd{$typ}) where F
       nrow, ncol = size(A)
-      colptr = A.colptr .- 1  # 0-based indexing
-      rowval = A.rowval .- 1
+      colptr = A.colptr .- $typ(1)  # 0-based indexing
+      rowval = A.rowval .- $typ(1)
       p = zeros($typ, nrow+1) # p is used as a workspace during the ordering, which is why it must be of length n+1, not just n
       valid = ccall($fn, $typ,
                     ($typ, Ref{$typ}, Ref{$typ}, Ptr{$typ}, Ptr{Cdouble},  Ptr{$typ},                                   Ptr{Cvoid},                           Ptr{Cvoid}),
                      nrow,    rowval,    colptr,         p,   meta.knobs, meta.stats, @cfunction(calloc, Ptr{Cvoid}, ($typ, $typ)), @cfunction(free, Cvoid, (Ptr{Cvoid},)))
       Bool(valid) || throw("symamd status: $(colamd_statuses[meta.stats[COLAMD_STATUS]])")
       pop!(p)
-      p .+= 1  # 1-based indexing
+      p .+= $typ(1)  # 1-based indexing
       return p
     end
 
