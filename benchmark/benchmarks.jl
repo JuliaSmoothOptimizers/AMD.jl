daf7e8b2e59b033dc8aa027cb7c19efb8ce41ff7
Monssaf Toukal <tmonssaf@gmail.com>
2021-01-14 11:16:26 -0500

    adding files to benchmark the module

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,9 +1,20 @@
+using BenchmarkTools
 using DelimitedFiles, LinearAlgebra, Printf, SparseArrays
 using Pkg.Artifacts
 
 using DataFrames, MatrixMarket
 using Metis, SymRCM, AMD
 using LDLFactorizations, SolverBenchmark
+
+const SUITE = BenchmarkGroup()
+SUITE["no_ordering"] = BenchmarkGroup()
+SUITE["amd"] = BenchmarkGroup()
+SUITE["symamd"] = BenchmarkGroup()
+SUITE["metis"] = BenchmarkGroup()
+SUITE["symrcm"] = BenchmarkGroup()
+SUITE["nnz"] = BenchmarkGroup()
+
+
 
 latex_benchmarks = false
 
@@ -15,7 +26,7 @@ const sqd_hash = artifact_hash("sqdcollection", artifact_toml)
 const sqd_path = joinpath(artifact_path(sqd_hash), "sqd-collection-0.1")
 
 subdirs = readdir(sqd_path)
-const formulations = ("2x2", "3x3")
+const formulations = ["2x2"]#, "3x3"]
 
 names_sqd     = String[]
 ratio_amd     = Float64[]
@@ -40,51 +51,58 @@ for subdir ∈ subdirs
       push!(names_sqd, name)
       println(name)
 
-      p_amd      = amd(A)
-      p_symamd   = symamd(A)
-      p_metis, _ = Metis.permutation(A)
-      p_symrcm   = symrcm(A)
-      p_classic  = collect(1:size(A,1))
+      # p_amd      = amd(A)
+      # p_symamd   = symamd(A)
+      # p_metis, _ = Metis.permutation(A)
+      # p_symrcm   = symrcm(A)
+      # p_classic  = collect(1:size(A,1))
 
-      ldlt_amd     = ldl(A, p_amd)
-      ldlt_symamd  = ldl(A, p_symamd)
-      ldlt_metis   = ldl(A, p_metis)
-      ldlt_symrcm  = ldl(A, p_symrcm)
-      ldlt_classic = ldl(A, p_classic)
+      # ldlt_amd     = ldl(A, p_amd)
+      # ldlt_symamd  = ldl(A, p_symamd)
+      # ldlt_metis   = ldl(A, p_metis)
+      # ldlt_symrcm  = ldl(A, p_symrcm)
+      # ldlt_classic = ldl(A, p_classic)
 
-      push!(nnz_sqd, nnz_A)
-      push!(ratio_amd, nnz(ldlt_amd) / nnz_A)
-      push!(ratio_symamd, nnz(ldlt_symamd) / nnz_A)
-      push!(ratio_metis, nnz(ldlt_metis) / nnz_A)
-      push!(ratio_symrcm, nnz(ldlt_symrcm) / nnz_A)
-      push!(ratio_classic, nnz(ldlt_classic) / nnz_A)
+      # push!(nnz_sqd, nnz_A)
+      # push!(ratio_amd, nnz(ldlt_amd) / nnz_A)
+      # push!(ratio_symamd, nnz(ldlt_symamd) / nnz_A)
+      # push!(ratio_metis, nnz(ldlt_metis) / nnz_A)
+      # push!(ratio_symrcm, nnz(ldlt_symrcm) / nnz_A)
+      # push!(ratio_classic, nnz(ldlt_classic) / nnz_A)
+
+      SUITE["no_ordering"][name] = @benchmarkable collect(1:size($A,1))
+      SUITE["amd"][name] =  @benchmarkable amd($A)
+      SUITE["symamd"][name] = @benchmarkable symamd($A)
+      SUITE["metis"][name] = @benchmarkable Metis.permutation($A)
+      SUITE["symrcm"][name] = @benchmarkable symrcm($A)
+      SUITE["nnz"][name] = @benchmarkable nnz(tril($A))
     end
   end
 end
 
-orderings = [:NO_ORDERING, :AMD, :SYMAMD, :METIS, :SYMRCM]
-n = length(names_sqd)
-stats = Dict(ordering =>
-          DataFrame(
-            :id     => 1:n,
-            :name   => [@sprintf("%15s", names_sqd[i]) for i = 1:n],
-            :nnz    => [@sprintf("%6d", nnz_sqd[i]) for i = 1:n],
-            :ratio  => [@sprintf("%3.1f", (ordering == :AMD ? ratio_amd[i] : (ordering == :SYMAMD ? ratio_symamd[i] : (ordering == :METIS ? ratio_metis[i] : (ordering == :SYMRCM ? ratio_symrcm[i] : ratio_classic[i]))))) for i = 1:n]
-          ) for ordering in orderings)
+# orderings = [:NO_ORDERING, :AMD, :SYMAMD, :METIS, :SYMRCM]
+# n = length(names_sqd)
+# stats = Dict(ordering =>
+#           DataFrame(
+#             :id     => 1:n,
+#             :name   => [@sprintf("%15s", names_sqd[i]) for i = 1:n],
+#             :nnz    => [@sprintf("%6d", nnz_sqd[i]) for i = 1:n],
+#             :ratio  => [@sprintf("%3.1f", (ordering == :AMD ? ratio_amd[i] : (ordering == :SYMAMD ? ratio_symamd[i] : (ordering == :METIS ? ratio_metis[i] : (ordering == :SYMRCM ? ratio_symrcm[i] : ratio_classic[i]))))) for i = 1:n]
+#           ) for ordering in orderings)
 
-df = join(stats, [:ratio], invariant_cols=[:name, :nnz])
-hdr_override = Dict(:ratio_NO_ORDERING => "NO\\_ORDERING", :ratio_METIS => "METIS", :ratio_AMD => "AMD", :ratio_SYMRCM => "SYMRCM", :ratio_SYMAMD => "SYMAMD")
+# df = join(stats, [:ratio], invariant_cols=[:name, :nnz])
+# hdr_override = Dict(:ratio_NO_ORDERING => "NO\\_ORDERING", :ratio_METIS => "METIS", :ratio_AMD => "AMD", :ratio_SYMRCM => "SYMRCM", :ratio_SYMAMD => "SYMAMD")
 
-open("benchmarks.md", "w") do io
-  pretty_stats(io, df, hdr_override=hdr_override)
-end
+# open("benchmarks.md", "w") do io
+#   pretty_stats(io, df, hdr_override=hdr_override)
+# end
 
-if latex_benchmarks
-  open("benchmarks.tex", "w") do io
-    println(io, "\\documentclass[varwidth=20cm,crop=true]{standalone}")
-    println(io, "\\usepackage{longtable}")
-    println(io, "\\begin{document}")
-    pretty_latex_stats(io, df, hdr_override=hdr_override)
-    println(io, "\\end{document}")
-  end
-end
+# if latex_benchmarks
+#   open("benchmarks.tex", "w") do io
+#     println(io, "\\documentclass[varwidth=20cm,crop=true]{standalone}")
+#     println(io, "\\usepackage{longtable}")
+#     println(io, "\\begin{document}")
+#     pretty_latex_stats(io, df, hdr_override=hdr_override)
+#     println(io, "\\end{document}")
+#   end
+# end
