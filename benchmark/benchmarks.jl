de9bd37ce2d62358d5edac8b387da8e464dd5ced
Monssaf Toukal <tmonssaf@gmail.com>
2021-01-19 17:33:03 -0500

    corrections applied to benchmark suite

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -2,7 +2,7 @@ using BenchmarkTools
 using DelimitedFiles, LinearAlgebra, Printf, SparseArrays
 using Pkg.Artifacts
 
-using DataFrames, MatrixMarket
+using MatrixMarket
 using Metis, SymRCM, AMD
 using LDLFactorizations, SolverBenchmark
 
@@ -14,8 +14,6 @@ SUITE["metis"] = BenchmarkGroup()
 SUITE["symrcm"] = BenchmarkGroup()
 SUITE["nnz"] = BenchmarkGroup()
 
-
-
 latex_benchmarks = false
 
 # obtain path to SQD collection
@@ -26,7 +24,7 @@ const sqd_hash = artifact_hash("sqdcollection", artifact_toml)
 const sqd_path = joinpath(artifact_path(sqd_hash), "sqd-collection-0.1")
 
 subdirs = readdir(sqd_path)
-const formulations = ["2x2"]#, "3x3"]
+const formulations = ["2x2", "3x3"]
 
 names_sqd     = String[]
 ratio_amd     = Float64[]
@@ -45,37 +43,39 @@ for subdir ∈ subdirs
 
     A = MatrixMarket.mmread(joinpath(iterpath, "K_0.mtx"))
     b = readdlm(joinpath(iterpath, "rhs_0.rhs"), Float64)[:]
-    nnz_A = nnz(tril(A))
+    nnz_A = nnz(tril(A, -1))
+    n = size(A,1)
     if size(A, 1) ≤ 25000
       name = "$(subdir)_$(formulation)"
       push!(names_sqd, name)
+      
+      p_amd      = amd(A)
+      p_symamd   = symamd(A)
+      p_metis, _ = Metis.permutation(A)
+      p_symrcm   = symrcm(A)
+      p_classic  = collect(1:n)
       println(name)
-
-      # p_amd      = amd(A)
-      # p_symamd   = symamd(A)
-      # p_metis, _ = Metis.permutation(A)
-      # p_symrcm   = symrcm(A)
-      # p_classic  = collect(1:size(A,1))
-
+      
       # ldlt_amd     = ldl(A, p_amd)
       # ldlt_symamd  = ldl(A, p_symamd)
       # ldlt_metis   = ldl(A, p_metis)
       # ldlt_symrcm  = ldl(A, p_symrcm)
       # ldlt_classic = ldl(A, p_classic)
-
+      
       # push!(nnz_sqd, nnz_A)
       # push!(ratio_amd, nnz(ldlt_amd) / nnz_A)
       # push!(ratio_symamd, nnz(ldlt_symamd) / nnz_A)
       # push!(ratio_metis, nnz(ldlt_metis) / nnz_A)
       # push!(ratio_symrcm, nnz(ldlt_symrcm) / nnz_A)
       # push!(ratio_classic, nnz(ldlt_classic) / nnz_A)
-
-      SUITE["no_ordering"][name] = @benchmarkable collect(1:size($A,1))
-      SUITE["amd"][name] =  @benchmarkable amd($A)
-      SUITE["symamd"][name] = @benchmarkable symamd($A)
-      SUITE["metis"][name] = @benchmarkable Metis.permutation($A)
-      SUITE["symrcm"][name] = @benchmarkable symrcm($A)
-      SUITE["nnz"][name] = @benchmarkable nnz(tril($A))
+      
+      SUITE["no_ordering"][name] = @benchmarkable nnz(ldl($A, $p_classic).L)/$nnz_A
+      SUITE["amd"][name] =  @benchmarkable nnz(ldl($A, $p_amd).L)/$nnz_A
+      SUITE["symamd"][name] = @benchmarkable nnz(ldl($A, $p_symamd).L)/$nnz_A
+      SUITE["metis"][name] = @benchmarkable nnz(ldl($A, $p_metis).L)/$nnz_A
+      SUITE["symrcm"][name] = @benchmarkable nnz(ldl($A, $p_symrcm).L)/$nnz_A
+      SUITE["nnz"][name] = @benchmarkable nnz(tril($A, -1))
+      
     end
   end
 end
