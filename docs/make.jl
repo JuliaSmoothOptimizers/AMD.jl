58922064e8374cfaecb06a100374ddd84396f2c0
JSOBot <69814847+JSOBot@users.noreply.github.com>
2021-04-14 19:30:25 -0400

    adding/updating file: docs/make.jl

---
+++ docs/make.jl
@@ -0,0 +1,15 @@
+using Documenter, AMD
+
+makedocs(
+  modules = [AMD],
+  doctest = true,
+  # linkcheck = true,
+  strict = true,
+  format = Documenter.HTML(assets = ["assets/style.css"], prettyurls = get(ENV, "CI", nothing) == "true"),
+  sitename = "AMD.jl",
+  pages = Any["Home" => "index.md",
+              "Tutorial" => "tutorial.md",
+              "Reference" => "reference.md"]
+)
+
+deploydocs(repo = "github.com/JuliaSmoothOptimizers/AMD.jl.git")
