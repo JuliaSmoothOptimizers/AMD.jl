402547a22b8c05beadf5531eafaae299bf19c5c4
Alexis Montoison <alexis.montoison@polymtl.ca>
2023-09-22 20:56:23 -0500

    [documentation] Update Documenter.jl

--- docs/make.jl
+++ docs/make.jl
@@ -3,8 +3,7 @@ using Documenter, AMD
 makedocs(
   modules = [AMD],
   doctest = true,
-  # linkcheck = true,
-  strict = true,
+  linkcheck = true,
   format = Documenter.HTML(
     assets = ["assets/style.css"],
     prettyurls = get(ENV, "CI", nothing) == "true",
